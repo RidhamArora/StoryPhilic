d3d50ee5b374e41d19606182b19a9297d3859830
RidhamArora <ridham0001@hotmail.com>
2018-07-13 03:14:26 +0530

    handled auth

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const passport = require('passport');
+const auth = require('./routes/auth');
+require('./config/passport')(passport);
 const app = express();
 
 const port = process.env.PORT || 5000;
@@ -8,6 +10,8 @@ const port = process.env.PORT || 5000;
 app.get('/', (req,res) => {
     res.send('It Works!');
 });
+
+app.use('/auth', auth);
 
 app.listen(port, () => {
     console.log('Server started on port ${port}')
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:'mongodb://<dbuser>:<dbpassword>@ds235431.mlab.com:35431/story-of-books-dev',
+    googleClientID:'954187382772-4hpofnkpnqaa1sealfqn3iemsp917grq.apps.googleusercontent.com',
+    googleClientSecret:'d5NV_FpjCKeASoYDI_5j_I0l'
+}
