ecedb21f3b476e3b19600be5bf620db7fcb4e523
RidhamArora <ridham0001@hotmail.com>
2018-07-13 19:44:13 +0530

    Handled UI with Google Auth

--- app.js
+++ app.js
@@ -1,18 +1,53 @@
 const express = require('express');
+const exphbs   = require('express-handlebars');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const auth = require('./routes/auth');
+const index = require('./routes/index');
+require('./models/User');
 require('./config/passport')(passport);
+
 const app = express();
+
+app.engine('handlebars' , exphbs({
+    defaultLayout:'main'
+}));
+
+app.set('view engine','handlebars');
+
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
+
+const keys = require('./config/keys');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(keys.mongoURI ,{
+    useNewUrlParser : true
+})
+   .then(() => console.log("MongoDB Connected"))
+   .catch(err => console.log(err) );
 
 const port = process.env.PORT || 5000;
 
-app.get('/', (req,res) => {
-    res.send('It Works!');
-});
+app.use(cookieParser());
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize());
+app.use(passport.session());
 
+app.use((req, res, next) => {
+    res.locals.user = req.user || null;
+    next();
+});
+app.use('/',index);
 app.use('/auth', auth);
 
 app.listen(port, () => {
-    console.log('Server started on port ${port}')
+    console.log('Server started')
 });
--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI:'mongodb://<dbuser>:<dbpassword>@ds235431.mlab.com:35431/story-of-books-dev',
+    mongoURI:'mongodb://Ridham:43mani21@ds235431.mlab.com:35431/story-of-books-dev',
     googleClientID:'954187382772-4hpofnkpnqaa1sealfqn3iemsp917grq.apps.googleusercontent.com',
     googleClientSecret:'d5NV_FpjCKeASoYDI_5j_I0l'
 }
